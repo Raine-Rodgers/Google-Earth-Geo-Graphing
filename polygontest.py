40330e39b21b9082c4303306ae8dbf5435a889d9
Raine <80836177+Your-Average-Arsonist@users.noreply.github.com>
2023-11-19 21:37:55 +0100

    got shit working

--- polygontest.py
+++ polygontest.py
@@ -1,7 +1,26 @@
 import simplekml
+
+# Create a new KML object
 kml = simplekml.Kml()
-pol = kml.newpolygon(name='A Polygon')
-pol.outerboundaryis = [(18.333868,-34.038274), (18.370618,-34.034421),
-                       (18.350616,-34.051677),(18.333868,-34.038274)]
+
+x = 8.47287495092937
+y = 50.2237550498698
+z = 1000
+
+coordinates = [
+    (x+0.0002, y+0.0002, 1000),
+    (x-0.0002, y+0.0002, 1000),
+    (x-0.0002, y-0.0001, 1000),
+    (x+0.0002, y-0.0001, 1000),
+    (x+0.0002, y+0.0002, 1000)
+]
+
+# Create a new polygon with the defined coordinates
+pol = kml.newpolygon(name="Polygon", outerboundaryis=coordinates)
+
+# Set the polygon style
+pol.extrude = 1
 pol.altitudemode = simplekml.AltitudeMode.relativetoground
-kml.save("Polygon.kml")
+
+# Save the KML object to a file
+kml.save("output.kml")
