7b8c47e7e79ca9d89d2b29ab93b1225751437a2b
Raine <80836177+Your-Average-Arsonist@users.noreply.github.com>
2023-11-27 21:55:10 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
-from Pyside6.QtWidgets import QApplication, QWidget
+#Importing the components we need
+from PySide6.QtWidgets import QApplication, QWidget
+
+#The sys module is responsible for processing commmand line arguments
 import sys
 
 app = QApplication(sys.argv)
+
+window = QWidget()
+window.show()
+
+#Start the event loop
+app.exec()
