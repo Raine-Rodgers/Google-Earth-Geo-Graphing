564262064f53affe93f25242d2c11a98fd634cba
Raine <80836177+Your-Average-Arsonist@users.noreply.github.com>
2023-12-04 10:13:05 +0100

    test

--- app.py
+++ app.py
@@ -1,10 +1,2 @@
-import kivy
-from kivy.app import App
-from kivy.uix.label import Label
-
-class MyApp(App):
-    def build(self):
-        return Label(text="hello World")
-
-if __name__ == '__main__':
-    MyApp().run()
+import PySide6.QtWidgets
+import sys
