43f3dfb46b1992513ce7b465446975054eb6ac38
Raine <80836177+Your-Average-Arsonist@users.noreply.github.com>
2023-12-06 14:56:17 +0100

    put maine functions into simple methods

--- polygontest.py
+++ polygontest.py
@@ -3,11 +3,16 @@ import simplekml
 # Create a new KML object
 kml = simplekml.Kml()
 
+# get name from title page
 name = "polygon"
 
+# make sure these work for different scales
 XPreset = 8.47287495092937
 YPreset = 50.2237550498698
 ZPreset = 1000
+
+coordinates = [[None] * 4] * 3
+# sets up coordinates
 def createCoords():
     coordinates = [
         (XPreset+0.0002, YPreset+0.0002, ZPreset),
@@ -17,13 +22,16 @@ def createCoords():
         (XPreset+0.0002, YPreset+0.0002, ZPreset)
     ]
 
-createCoord()
+# sets up and saves file
+def saveFile(coordinates):
+    pol = kml.newpolygon(name=name, outerboundaryis=coordinates)
+    # Set the polygon style
+    pol.extrude = 1
+    pol.altitudemode = simplekml.AltitudeMode.relativetoground
+    # Save the KML object to a file
+    kml.save("output.kml")
+
 # Create a new polygon with the defined coordinates
-pol = kml.newpolygon(name=name, outerboundaryis=coordinates)
+createCoords()
 
-# Set the polygon style
-pol.extrude = 1
-pol.altitudemode = simplekml.AltitudeMode.relativetoground
-
-# Save the KML object to a file
-kml.save("output.kml")
+saveFile(coordinates)
