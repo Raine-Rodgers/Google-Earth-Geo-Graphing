e8ddfe9f1467a6549aa54897083cf65e6218b151
Raine <80836177+Your-Average-Arsonist@users.noreply.github.com>
2023-12-07 12:57:43 +0100

    made oop kinda work now

--- src/polygontest.py
+++ src/polygontest.py
@@ -1,34 +1,37 @@
 import simplekml
 
-class MakeFile():
-    def __init__(self, kml, fileName, XPreset, YPreset, ZPreset, coordinates):
-        self.kml = simplekml.Kml() # Create a new KML object
-        self.fileName = "polygon" # get name from title page
-        self.XPreset = 8.47287495092937 # make sure these work for different scales
-        self.YPreset = 50.2237550498698 # make sure these work for different scales
-        self.ZPreset = 1000 # make sure these work for different scales
-        self.coordinates = [[None]]
+class MakeFile:
+    def __init__(self):
+        self.kml = simplekml.Kml()
+        self.coords = []
 
 # sets up coordinates
-def createCoords():
-    coordinates = [
-        (XPreset+0.0002, YPreset+0.0002, ZPreset),
-        (XPreset-0.0002, YPreset+0.0002, ZPreset),
-        (XPreset-0.0002, YPreset-0.0001, ZPreset),
-        (XPreset+0.0002, YPreset-0.0001, ZPreset),
-        (XPreset+0.0002, YPreset+0.0002, ZPreset)
-    ]
+    def createCoords(self, x, y, z):
+        self.coords.append((x, y, z))
 
 # sets up and saves file
-def saveFile(coordinates):
-    pol = kml.newpolygon(name=name, outerboundaryis=coordinates)
-    # Set the polygon style
-    pol.extrude = 1
-    pol.altitudemode = simplekml.AltitudeMode.relativetoground
-    # Save the KML object to a file
-    kml.save("output.kml")
+    def saveFile(self, filename, name, coordinates):
+        if self.coords:
+            pol = self.kml.newpolygon(name=name, outerboundaryis=coordinates)
+            pol.extrude = 1
+            pol.altitudemode = simplekml.AltitudeMode.relativetoground
+            self.kml.save(filename + ".kml")
+        else:
+            print("No coordinates to save.")
+
+    # pol = kml.newpolygon(name=name, outerboundaryis=coordinates)
+    # # Set the polygon style
+    # pol.extrude = 1
+    # pol.altitudemode = simplekml.AltitudeMode.relativetoground
+    # # Save the KML object to a file
+    # kml.save("output.kml")
 
 # Create a new polygon with the defined coordinates
-createCoords()
+    createCoords(3, 5, 10)
 
-saveFile(coordinates)
+    saveFile("Test", "Name", 5)
+
+
+
+
+
