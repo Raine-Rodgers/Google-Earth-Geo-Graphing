bec10dab337c5b6c1ea9ec4c1f27c6aad5a2c2ce
Raine <80836177+Your-Average-Arsonist@users.noreply.github.com>
2023-12-09 20:10:50 +0100

    made some changes

--- src/polygontest.py
+++ src/polygontest.py
@@ -1,31 +1,18 @@
-import simplekml
 
-class MakeFile:
-    def __init__(self):
-        self.kml = simplekml.Kml()
-        self.coords = []
+class CreateCoordinates:
+    def __init__(self, x, y, z, name):
+        self.__name = name
+        self.__x = x
+        self.__y = y
+        self.__z = z
 
-# sets up coordinates
-    def createCoords(self, x, y, z):
-        self.coords.append((x, y, z))
-
-# sets up and saves file
-    def saveFile(self, filename, name, coordinates):
-        if self.coords:
-            pol = self.kml.newpolygon(name=name, outerboundaryis=coordinates)
-            pol.extrude = 1
-            pol.altitudemode = simplekml.AltitudeMode.relativetoground
-            self.kml.save(filename + ".kml")
-        else:
-            print("No coordinates to save.")
-
-    # pol = kml.newpolygon(name=name, outerboundaryis=coordinates)
-    # # Set the polygon style
-    # pol.extrude = 1
-    # pol.altitudemode = simplekml.AltitudeMode.relativetoground
-    # # Save the KML object to a file
-    # kml.save("output.kml")
-
-# Create a new polygon with the defined coordinates
-    createCoords(3, 5, 10, 5)
-    saveFile("Test", "Name", self.coords)
+    def getX():
+        return x
+    
+    def getY():
+        return y
+    
+    def getZ():
+        return z
+    
+    pol = kml.newpolygon(name=name, outerboundaryis=coordinates)
---
+++ src/testrunpleaseworkdotfilehelp.py
@@ -0,0 +1,1 @@
+from polygontest import *
