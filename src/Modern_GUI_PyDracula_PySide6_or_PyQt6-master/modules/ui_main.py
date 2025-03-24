8db9b46e05982dfc84ad7682b12f01cae07bcb50
Raine <80836177+Raine-Rodgers@users.noreply.github.com>
2025-03-24 11:42:24 +0100

    made ui changes to make the window smaller on spawn

--- src/Modern_GUI_PyDracula_PySide6_or_PyQt6-master/modules/ui_main.py
+++ src/Modern_GUI_PyDracula_PySide6_or_PyQt6-master/modules/ui_main.py
@@ -18,7 +18,7 @@ class Ui_MainWindow(object):
         def setupUi(self, MainWindow):
                 if not MainWindow.objectName():
                         MainWindow.setObjectName(u"MainWindow")
-                MainWindow.resize(1280, 720)
+                MainWindow.resize(710, 360) #1280, 720
                 MainWindow.setMinimumSize(QSize(940, 560))
                 self.styleSheet = QWidget(MainWindow)
                 self.styleSheet.setObjectName(u"styleSheet")
